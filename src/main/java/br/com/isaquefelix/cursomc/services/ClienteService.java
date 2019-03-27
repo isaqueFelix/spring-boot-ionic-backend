8d8fe9a9577b8734584208d0a0da39bfa7a2dc88
Isaque-Felix1 <isaque.felix1@ibm.com>
2019-03-27 16:08:07 -0300

    endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/isaquefelix/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.isaquefelix.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.isaquefelix.cursomc.domain.Cliente;
+import br.com.isaquefelix.cursomc.repositories.ClienteRepository;
+import br.com.isaquefelix.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
+				", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
