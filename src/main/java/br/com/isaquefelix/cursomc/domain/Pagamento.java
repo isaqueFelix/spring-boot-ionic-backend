3bedfcfab39a9a24059f41ec5edd327bcc643c4b
Isaque-Felix1 <isaque.felix1@ibm.com>
2019-03-28 11:04:22 -0300

    atualizacao para utilizar apenas jsonignore

--- src/main/java/br/com/isaquefelix/cursomc/domain/Pagamento.java
+++ src/main/java/br/com/isaquefelix/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.isaquefelix.cursomc.domain.enums.EstadoPagamento;
 
@@ -27,7 +27,7 @@ public abstract class Pagamento  implements Serializable {
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
-	@JsonBackReference
+	@JsonIgnore
 	private Pedido pedido;
 	
 	public Pagamento () {}
