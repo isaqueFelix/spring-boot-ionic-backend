dd98c563122b35cfc328c79d6087587d658f4b34
Isaque-Felix1 <isaque.felix1@ibm.com>
2019-03-29 14:12:48 -0300

    personalizando o  retorno da validacao

---
+++ src/main/java/br/com/isaquefelix/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package br.com.isaquefelix.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
