f92441a1cd6c465e867fd11285228abaed106496
Isaque-Felix1 <isaque.felix1@ibm.com>
2019-06-11 15:00:35 -0300

    implementando autorizacao

--- src/main/java/br/com/isaquefelix/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/isaquefelix/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.isaquefelix.cursomc.security.JWTAuthenticationFilter;
+import br.com.isaquefelix.cursomc.security.JWTAuthorizationFilter;
 import br.com.isaquefelix.cursomc.security.JWTUtils;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
