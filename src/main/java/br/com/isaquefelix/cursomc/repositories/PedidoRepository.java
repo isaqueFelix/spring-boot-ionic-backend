388593c5c73f506933443e1dd60293ea89866fc7
Isaque-Felix1 <isaque.felix1@ibm.com>
2019-06-11 17:07:13 -0300

    cliante recupera os seus pedidos

--- src/main/java/br/com/isaquefelix/cursomc/repositories/PedidoRepository.java
+++ src/main/java/br/com/isaquefelix/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,16 @@
 package br.com.isaquefelix.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.isaquefelix.cursomc.domain.Cliente;
 import br.com.isaquefelix.cursomc.domain.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 	
-	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, PageRequest pageRequest);
 }
